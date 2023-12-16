76fca97450171fde087f96d1ff64c27922c3912e
David Guerra <davidguerra@Davids-MacBook-Pro-2.local>
2023-12-16 13:15:26 -0500

    docstring and debug

--- src/App.js
+++ src/App.js
@@ -1,61 +1,23 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar';
-import RepoGraph from './components/RepoGraph/RepoGraph';
-import './App.css'; // Global styles
+import './App.css'; // Imports global styles for the application
 
+/**
+ * App Component
+ * 
+ * This is the main component of the application. It serves as the root container
+ * and includes the SearchBar and a container for displaying graphs.
+ */
 function App() {
-  // Initial dummy repositories state
-  const [repos, setRepos] = useState([
-    { id: 1, name: 'Repo 1', commits: [10, 20, 30], color: 'black' },
-    // Add more dummy repo data if necessary
-  ]);
-
-  // Dummy data for RepoGraph
-  const commitData = {
-    labels: ['Week 1', 'Week 2', 'Week 3'], // This should be dynamic based on actual data
-    datasets: repos.map(repo => ({
-      label: repo.name,
-      data: repo.commits, // Array of commit counts for each week
-      borderColor: repo.color, // Use the repo's color
-      backgroundColor: 'rgba(255, 255, 255, 0.5)', // Optional: Set background color to a semi-transparent white if you want
-      pointBackgroundColor: 'white', // Points color to white
-      pointBorderColor: 'white', // Points border color to white
-    })),
-  };
-
-  // Function to handle search and add repo to the graph
-  const handleSearch = (searchTerm) => {
-    // You would implement the API call and state update here
-    // For now, let's just add a dummy repo to the list when searching
-    const newRepo = {
-      id: repos.length + 1,
-      name: `Repo ${repos.length + 1}`,
-      commits: [Math.random() * 100, Math.random() * 100, Math.random() * 100],
-      color: `#${Math.floor(Math.random()*16777215).toString(16)}`, // Generate a random color
-    };
-    setRepos([...repos, newRepo]);
-  };
-
-  // Function to handle removal of a repo from the list and graph
-  const handleRemoveRepo = (repoId) => {
-    // You would implement the removal logic here
-    setRepos(repos.filter(repo => repo.id !== repoId));
-  };
-
   return (
     <div className="app-container">
-      <div className="graph-container">
-        {/* Pass the dynamic commitData based on the repos state */}
-        {/*<RepoGraph commitData={commitData} />*/}
-      </div>
-      
-      {/* Pass the handleSearch function to SearchBar */}
-      <SearchBar onSearch={handleSearch} />
-      
+      {/* Container for graph visualization (to be implemented) */}
+      <div className="graph-container"></div>
+
+      {/* SearchBar component for searching and displaying GitHub repositories */}
+      <SearchBar />
     </div>
   );
 }
 
 export default App;
-
--- webpack.config.js
+++ webpack.config.js
@@ -6,57 +6,70 @@ const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 require('dotenv').config();
 
+/**
+ * Webpack Configuration
+ * 
+ * This configuration file sets up Webpack for bundling a React application for production.
+ * It includes settings for output, optimization, plugins, module rules, and the development server.
+ */
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
+  mode: 'production', // Sets the mode to production for optimized bundles
+  entry: './src/index.js', // Entry point for the application
+
+  // Output configuration for the bundled files
   output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js', // Name of the output file with content hash
+    path: path.resolve(__dirname, 'dist'), // Output directory
   },
+
+  // Optimization settings for minimizing the output and splitting chunks
   optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    minimize: true, // Enables minimization
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()], // Minimizers for JavaScript and CSS
     splitChunks: {
-      chunks: 'all',
+      chunks: 'all', // Splits all types of chunks for optimization
     },
   },
+
+  // Plugins for additional functionality
   plugins: [
-    new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(), // Cleans up the output directory before each build
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      template: './public/index.html', // Template file for the HTML output
     }),
-    new webpack.DefinePlugin({
-        'process.env.REACT_APP_GOOGLE_CLOUD_API_KEY': JSON.stringify(process.env.REACT_APP_GOOGLE_CLOUD_API_KEY),
-      }),
-    new webpack.DefinePlugin({
-        'process.env.REACT_APP_GITHUB_TOKEN': JSON.stringify(process.env.REACT_APP_GITHUB_TOKEN),
-      }),
+    new webpack.DefinePlugin({ // DefinePlugin to make environment variables available
+      'process.env.REACT_APP_GOOGLE_CLOUD_API_KEY': JSON.stringify(process.env.REACT_APP_GOOGLE_CLOUD_API_KEY),
+      'process.env.REACT_APP_GITHUB_TOKEN': JSON.stringify(process.env.REACT_APP_GITHUB_TOKEN),
+    }),
   ],
+
+  // Module rules for handling different file types
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // Regex to test for .js or .jsx files
-        exclude: /node_modules/,
+        test: /\.(js|jsx)$/, // Matches JavaScript and JSX files
+        exclude: /node_modules/, // Excludes node_modules directory
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // Uses babel-loader for transpiling JavaScript
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'] // Presets for both modern JS and React
+            presets: ['@babel/preset-env', '@babel/preset-react'] // Presets for modern JavaScript and React
           }
         }
       },
       {
-        test: /\.css$/, // Regex to match CSS files
-        use: ['style-loader', 'css-loader'], // Use these loaders
+        test: /\.css$/, // Matches CSS files
+        use: ['style-loader', 'css-loader'], // Uses style-loader and css-loader for CSS files
       },
     ]
   },
+
+  // Development server configuration
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public'),
+      directory: path.join(__dirname, 'public'), // Directory to serve static files from
     },
-    compress: true,
-    port: 3000,
-    open: true, 
+    compress: true, // Enables gzip compression
+    port: 3000, // Port on which to run the server
+    open: true, // Automatically opens the browser when the server starts
   },
-  
 };
